f024abd7c696554d1fcd545ac77e0c4a287987cd
Diana Tudosa <diana.tudosa@tremend.com>
2020-04-27 19:02:20 +0300

    Com

--- components/new.tsx
+++ components/new.tsx
@@ -7,7 +7,36 @@ import AssetUtils from 'expo-asset-utils';
 import * as styleConstants from '../utils/styles'
 import FullWidthImage from 'react-native-fullwidth-image'
 import { StorageTypes, Routes }from '../utils/enums';
+/*
 
+registerForPushNotifications: async function() {
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
+
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== "granted") {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const { status } = await Permissions.askAsync(
+      Permissions.NOTIFICATIONS
+    );
+    finalStatus = status;
+  }
+
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== "granted") {
+    return;
+  }
+
+  // Get the token that uniquely identifies this device
+  Notifications.getExpoPushTokenAsync().then(token => {
+    console.log(token);
+  });
+}
+*/
 
 const New = ({ navigation }) => {
   const [image, setImage] = useState(null);
